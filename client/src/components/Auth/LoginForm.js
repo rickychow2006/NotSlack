7e83f6f0d3cbf2547d6844c7bba9e0cc50cd7979
rickychow2006 <rickychow0716@gmail.com>
2021-12-22 15:45:57 -0600

    created login form

--- client/src/components/Auth/LoginForm.js
+++ client/src/components/Auth/LoginForm.js
@@ -1,6 +1,62 @@
 import {useState} from "react";
 import { useSelector, useDispatch} from "react-redux";
 import { login } from "../../store/session";
-import useConsumeContext from "../../context/FormalModalContext";
 import "./Form.css";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const user = useSelector(state => state.session.user);
+  const dispatch = useDispatch(); 
+  
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login(username, password));
+    if (data.errors) {
+      setErrors(data.errors);
+    }
+  };
+
+  return (
+    <div className="form__wrapper">
+      <form onSubmit={handleLogin}> 
+        <div className="form__header">
+          <h1>Login</h1>
+        </div>
+        <div className="errors">
+          {errors?.map((error) => (
+            <div key={error}>・{error}</div>
+          ))}
+        </div>
+        <div className="form__input">
+          <input 
+              type="text"
+              name="username"
+              placeholder="Please Type Your Username..."
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+          >
+          </input>
+        </div>
+        <div className="form__input">
+          <input
+              type="password"
+              name="password"
+              placeholder="Input PassWord..."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+          >
+          </input>
+        </div>
+        <div className='form__button'>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
+
 
